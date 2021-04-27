fcfd76db4eaefad9011289083ef84e51f384ffa8
Abdow <abdowvg69@gmail.com>
2021-04-27 20:19:50 +0200

    added loading index

--- models/distilbert.py
+++ models/distilbert.py
@@ -4,6 +4,8 @@ from models.model import myModel
 import os
 from sentence_transformers import SentenceTransformer
 import torch
+import ngtpy
+
 
 class DistilBert(myModel):
 
@@ -20,45 +22,66 @@ class DistilBert(myModel):
         self.name = 'DistilBert'
         self.model = SentenceTransformer('msmarco-distilbert-base-v3')
         self.model = self.model.to(device)
+        self.index = None
+        self.searcher = SimpleSearcher.from_prebuilt_index("msmarco-passage")
+        self.mapping = dict()
+        for i in range(self.searcher.num_docs):
+            self.mapping[i] = self.searcher.doc(i).docid()
 
             
     #must have a fast GPU and a big RAM storage
     def generate_embeddings(self,folder,batch_size=1000000):
         print("Warning : heavy operation , must have fast GPU and sufficent RAM storage...")
-        searcher = SimpleSearcher.from_prebuilt_index("msmarco-passage")
+        
         j = 1
         batch_emb = []
-        for i in range(searcher.num_docs):
+        for i in range(self.searcher.num_docs):
+            chaine=self.searcher.doc(i).raw()
+            chaine=json.loads(chaine)['contents']
+            batch_emb.append(chaine)
             if (i+1) % batch_size == 0:
                 print("encoding batch : ",j)
                 emb = self.model.encode(batch_emb)
                 np.save(folder+"/msmarco-passage"+str(j)+".npy", emb)
-                print(i," documens done.")
+                print(i+1," documens done.")
                 emb = None
                 j+=1
                 batch_emb = []
-            chaine=searcher.doc(i).raw()
-            chaine=json.loads(chaine)['contents']
-            batch_emb.append(chaine)
-
         emb = self.model.encode(batch_emb)
         np.save(folder+"/msmarco-passage"+str(j)+".npy", emb)
-                
-                
-    def get_scorces_query(self, id,query,k):
-        oldrun = run_parser(self.firststage)
-        q_results = oldrun.get(id,dict())
-        out = dict()
+    
+    #must respect the generation conditions
+    def generate_index(self,folder,index_path):
+        n = len(os.listdir("file"))
+        ngtpy.create(index_path, 768, distance_type='Cosine')
+        index = ngtpy.Index(index_path)
+        j=0
+        for i in range(n):
+            liste = np.load(folder+"/msmarco-passage"+str(i)+".npy",mmap_mode='r')
+            for l in liste:
+                l = list(map(float,list(l)))
+                index.insert(l) # insert objects
+                j+=1
+                if j % 100000 == 0:
+                    print(str(j)+" element indexted")
+            liste = None
+        print('building objects...')
+        index.build_index()
+        print('saving the index...')
+        index.save()    
         
-        for key,score in q_results.items():
+        
+    def load_index(self,index_path):
+        self.index = ngtpy.Index(index_path)
+        
+    def from_prebuilt_index():
+        pass
+            
+    def get_scorces_query(self, id,query,k):
+        out = dict()
+        emb = self.model.encode(query)
+        hits = self.index.search(emb,k,0.01)
+        for hit in hits:
+            out[self.mapping[hit[0]]] = 1-hit[1]
 
-            query = prepareText(query)
-
-            chaine=self.searcher.doc(key).raw()
-            chaine=json.loads(chaine)['contents']
-            chaine = prepareText(chaine)
-
-            out[key] = cosine_similarity(self.model.get_sentence_vector(query),self.model.get_sentence_vector(chaine))
-
-        sor = sorted(out.items(),reverse = True, key=lambda x: x[1])
-        return dict(sor)
+        return out

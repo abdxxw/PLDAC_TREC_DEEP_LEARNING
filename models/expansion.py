8aef5cbd5a21561f76ceab2dec8b49bbc9b339c7
sihamdmostafa <gm_sihamdi@esi.dz>
2021-05-26 23:39:58 +0200

    some_bugs

--- models/expansion.py
+++ models/expansion.py
@@ -3,27 +3,26 @@ import json
 import os
 import gdown
 
-def convert_collection_to_json(collection_path,output_folder,max_docs_per_file=1000000):
+def convert_collection_to_json(path,Foutput,max_docs_per_file=1000000):
     ####convert collection to json###########
-    file_index = 0
-    with open(collection_path, encoding='utf-8') as f:
+    index_fichier = 0
+    with open(path, encoding='utf-8') as f:
         for i, line in enumerate(f):
             doc_id, doc_text = line.rstrip().split('\t')
             if i % max_docs_per_file == 0:
                 if i > 0:
-                    output_jsonl_file.close()
-                output_path = os.path.join(output_folder, 'docs{:02d}.json'.format(file_index))
-                output_jsonl_file = open(output_path, 'w', encoding='utf-8', newline='\n')
-                file_index += 1
+                    res.close()
+                output_path = os.path.join(Foutput, 'docs{:02d}.json'.format(index_fichier))
+                res = open(output_path, 'w', encoding='utf-8', newline='\n')
+                index_fichier += 1
             output_dict = {'id': doc_id, 'contents': doc_text}
-            output_jsonl_file.write(json.dumps(output_dict) + '\n')
+            res.write(json.dumps(output_dict) + '\n')
 
             if i % 100000 == 0:
                 print(i)
-    output_jsonl_file.close() 
+    res.close() 
     
 
-# stopwords
 stop_words = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there',
               'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they',
               'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into',
@@ -43,65 +42,63 @@ stop_words = {'ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there
 
 
 def prepare(text):
-    # process text by tokenizing and removing stopwords
-    processed = text.lower().replace('.', ' ').replace(',', ' ').replace('?', ' ')
-    return [word for word in processed.split() if word not in stop_words]
+    prepoc = text.lower().replace('.', ' ').replace(',', ' ').replace('?', ' ')
+    return [word for word in prepoc.split() if word not in stop_words]
 
 
 
 def split_new_repeated(pred_text, doc_text):
     # split new and repeated prediction words
-    pred_repeated = []
+    pred_r = []
     pred_new = []
 
     doc_text_set = set(prepare(doc_text))
     processed_pred_text = prepare(pred_text)
     for word in processed_pred_text:
         if word in doc_text_set:
-            pred_repeated.append(word)
+            pred_r.append(word)
         else:
             pred_new.append(word)
 
-    return pred_new, pred_repeated
+    return pred_new, pred_r
 
 
-def create_passage_expansion(output_folder,collection_path,predictions,max_docs_per_file=1000000,new_prediction_copies=1,split_predictions=False,repeated_prediction_copies=1,original_copies=1,prediction_copies=1) :
-    file_index = 0
-    new_words = 0
-    total_words = 0
+def create_passage_expansion(Foutput,path,pred,max_docs_per_file=1000000,new_pred=1,s_predictions=False,r_prediction_c=1,orig_copies=1,prediction_c=1) :
+    index_fichier = 0
+    words_n = 0
+    nombre_words = 0
 
-    with open(collection_path) as f_corpus, open(predictions) as f_pred:
+    with open(path) as f_corpus, open(pred) as f_pred:
         for i, (line_doc, line_pred) in enumerate(zip(f_corpus, f_pred)):
-            # Write to a new file when the current one reaches maximum capacity.
             if i % max_docs_per_file == 0:
                 if i > 0:
-                    output_jsonl_file.close()
-                output_path = os.path.join(output_folder, f'docs{file_index:02d}.json')
-                output_jsonl_file = open(output_path, 'w')
-                file_index += 1
+                    res.close()
+                output_path = os.path.join(Foutput, f'docs{index_fichier:02d}.json')
+                res = open(output_path, 'w')
+                index_fichier += 1
 
             doc_id, doc_text = line_doc.rstrip().split('\t')
             pred_text = line_pred.rstrip()
 
-            contents = ''
-            if split_predictions:
+            contenu = ''
+            if s_predictions:
                 pred_new, pred_repeated = split_new_repeated(pred_text, doc_text)
-                new_words += len(pred_new)
-                total_words += len(pred_new) + len(pred_repeated)
+                words_n += len(pred_new)
+                nombre_words += len(pred_new) + len(pred_repeated)
 
-                contents += (doc_text + ' ') * original_copies
-                contents += (' '.join(pred_repeated) + ' ') * repeated_prediction_copies
-                contents += (' '.join(pred_new) + ' ') * new_prediction_copies
+                contenu += (doc_text + ' ') * orig_copies
+                contenu += (' '.join(pred_repeated) + ' ') * r_prediction_c
+                contenu += (' '.join(pred_new) + ' ') * new_pred
             else:
-                contents += (doc_text + ' ') * original_copies
-                contents += (pred_text + ' ') * prediction_copies
+                contenu += (doc_text + ' ') * orig_copies
+                contenu += (pred_text + ' ') * prediction_c
 
-            output_dict = {'id': doc_id, 'contents': contents}
-            output_jsonl_file.write(json.dumps(output_dict) + '\n')
+            output_dict = {'id': doc_id, 'contenu': contenu}
+            res.write(json.dumps(output_dict) + '\n')
 
             if i % 100000 == 0:
                 print(i)
-    output_jsonl_file.close()
+    res.close()
     
     
 
